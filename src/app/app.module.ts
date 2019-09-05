f75677d3c07f67d3d5cf205b94b49e29c8cae9d7
Abdallah <abdallah@Locals-MacBook-Pro.local>
2019-09-05 23:29:53 +0200

    Buttons added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ZoomPageTemplateComponent } from '@zoomui/page-template';
+import { ZoomButtonComponent } from '@zoomui/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoomPageTemplateComponent,
+    ZoomButtonComponent
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    ZoomButtonComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
